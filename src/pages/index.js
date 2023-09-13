beda2b828a0b53dbd2a4ac319f8e4668b2fdf358
Joey Imlay <jo.imlay@gmail.com>
2023-09-13 10:30:32 +0100

    styling fixes on homepage

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,14 +8,24 @@ export default function Home() {
     <main className={styles.home}>
       <Header />
 
-      <h5>So, you want to travel to</h5>
-      <h1>Space</h1>
+      <section>
+        <h5>So, you want to travel to</h5>
+      </section>
 
-      <p>Let’s face it; if you want to go to space, you might as well genuinely go to
-      outer space and not hover kind of on the edge of it. Well sit back, and relax
-      because we’ll give you a truly out of this world experience!</p>
+      <section>
+        <h1>Space</h1>
+      </section>
 
-      <Explore />
+      <section>
+        <p>Let’s face it; if you want to go to space, you might as well genuinely go to
+        outer space and not hover kind of on the edge of it. Well sit back, and relax
+        because we’ll give you a truly out of this world experience!</p>
+      </section>
+
+      <section>
+        <Explore />
+      </section>
+
     </main>
   );
 }
