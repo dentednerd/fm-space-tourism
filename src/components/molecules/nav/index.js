078407f9cd0f7959ad4c3b5e3730f6ddb5df48db
Joey Imlay <jo.imlay@gmail.com>
2023-09-14 14:23:05 +0100

    styled nav for sm breakpoint

--- src/components/molecules/nav/index.js
+++ src/components/molecules/nav/index.js
@@ -5,15 +5,11 @@ import data from '../../../data/data.json';
 export default function Nav() {
   return (
     <nav className={styles.nav}>
-      <ul>
-        {["Home"].concat(Object.keys(data)).map(name => (
-          <li key={name}>
-            <Link href={name === "Home" ? `/` : `/${name}`}>
-              {name}
-            </Link>
-          </li>
-        ))}
-      </ul>
+      {["Home"].concat(Object.keys(data)).map(name => (
+        <Link key={name} href={name === "Home" ? `/` : `/${name}`}>
+          {name}
+        </Link>
+      ))}
     </nav>
   )
 }
